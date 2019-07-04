1983a3af5c2b9c4bc067bdfbd7e4531fad4e0e16
759729757 <759729757@qq.com>
2019-07-04 18:24:24 +0800

    二级页面

--- pages/resource/data.js
+++ pages/resource/data.js
@@ -1,243 +1,52 @@
-var data = {
-  "banner": [
-    {
-      "image": "https://images.unsplash.com/photo-1551214012-84f95e060dee?w=640",
-      "title": "Beginner's Guide",
-      "describe": "to Cinema 4D"
-    },
-    {
-      "image": "http://p1.so.qhimgs1.com/bdr/326__/t01d51d0555bd71a76d.jpg",
-      "title": "Beginner's Guide",
-      "describe": "to Cinema 4D"
-    },
-    {
-      "image": "http://p1.so.qhmsg.com/bdr/326__/t01d25127cf4c4dfa86.jpg",
-      "title": "Beginner's Guide",
-      "describe": "to Cinema 4D"
-    },
-  ],
-  "supplier": [
-    {
-      "image": "http://img4.imgtn.bdimg.com/it/u=2153937626,1074119156&fm=26&gp=0.jpg",
-      "name": "广州大酒店"
-    },
-    {
-      "image": "http://p0.so.qhimgs1.com/bdr/326__/t014c514ad3594d06ec.jpg",
-      "name": "广州大酒店2"
-    },
-    {
-      "image": "https://p.ssl.qhimg.com/dm/420_207_/t013bafbba6629b7604.jpg",
-      "name": "广州大酒店3"
-    },
-    {
-      "image": "https://ps.ssl.qhimg.com/dmfd/417_417_/t01256a07394a8ac97d.jpg",
-      "name": "广州大酒店4"
-    },
-    {
-      "image": "https://ps.ssl.qhimg.com/dmfd/420_207_/t01ed8e6f986b9c3356.jpg",
-      "name": "广州大酒店5"
-    }
-  ],
-  "viedo": [
-    {
-      "name": "LESS盟1",
-      "subName": "中国驰名商标企业",
-      "describe": "和广州其他的影视制 作公司也合作过，但",
-      "content": "33从领尚家居建材广再到联塑万嘉，每次的创意，和拍摄制作，新视团队都超越了我们的期待。我们已经合作3年了。",
-      "note": "印象：文案策划不错 演员资料很全",
-      "image": "https://p.ssl.qhimg.com/dm/664_417_/t01eaff13080a5f2829.jpg"
-    }, {
-      "name": "LES2",
-      "subName": "中国驰名商标企业",
-      "describe": "和广州其他的影视制 作公司也合作过，但",
-      "content": "22从领尚家居建材广告片到领尚生活品牌每次的创意，和拍摄制作，新视团队都超越了我们的期待。我们已经合作3年了。",
-      "note": "印象：文案策划不错 演员资料很全",
-      "image": "https://ps.ssl.qhimg.com/dm/417_417_/t01433ce62e3d6f5d32.jpg"
-    }, {
-      "name": "LE联盟3",
-      "subName": "中国驰名商标企业",
-      "describe": "和广州其他的影视制 作公司也合作过，但",
-      "content": "11从领尚家居建材牌形象片再到联塑万嘉，每次的创意，和拍摄制作，新视团队都超越了我们的期待。我们已",
-      "note": "印象：文案策划不错 演员资料很全",
-      "image": "https://p.ssl.qhimg.com/dm/420_207_/t01e897ef43bf4502f1.jpg"
-    }
-  ],
-  "media": [
-    {
-      "image": "https://p.ssl.qhimg.com/dm/420_207_/t01e897ef43bf4502f1.jpg",
-      "city": "广州",
-      "address": "天河区CBD",
-      "describe": "是一家尊崇“异智慧”创作理念的影视广告公司。",
-      "note": "SBSBSB"
-    }, {
-      "image": "https://p.ssl.qhimg.com/dm/664_417_/t01eaff13080a5f2829.jpg",
-      "city": "广州",
-      "address": "天河区CBD",
-      "describe": "是一家尊崇“异智慧”创作理念的影视广告公司。",
-      "note": "SBSBSB"
-    }, {
-      "image": "https://p.ssl.qhimg.com/dm/365_365_/t01b299246b4e983337.jpg",
-      "city": "广州",
-      "address": "天河区CBD",
-      "describe": "是一家尊崇“异智慧”创作理念的影视广告公司。",
-      "note": "SBSBSB"
-    }, {
-      "image": "http://img4.imgtn.bdimg.com/it/u=2153937626,1074119156&fm=26&gp=0.jpg",
-      "city": "广州",
-      "address": "天河区CBD",
-      "describe": "是一家尊崇“异智慧”创作理念的影视广告公司。",
-      "note": "SBSBSB"
-    }
-  ],
-  "hr": [
-    {
-      "image": "http://p0.so.qhimgs1.com/bdr/326__/t014c514ad3594d06ec.jpg",
-      "name": "陈田",
-      "describe": "舞台3D搭v 的 饭都不敢发那个"
-    }, {
-      "image": "http://img4.imgtn.bdimg.com/it/u=2153937626,1074119156&fm=26&gp=0.jpg",
-      "name": "陈田",
-      "describe": "舞台3D搭建v 繁华的 v 都不敢发那个"
-    }, {
-      "image": "http://p0.so.qhimgs1.com/bdr/326__/t014c514ad3594d06ec.jpg",
-      "name": "陈田",
-      "describe": "舞台3D搭建v 繁华都不敢发那个"
-    }, {
-      "image": "http://img4.imgtn.bdimg.com/it/u=2153937626,1074119156&fm=26&gp=0.jpg",
-      "name": "陈田",
-      "describe": "舞台3D搭 饭都不敢发那个"
-    }
-  ],
-  "performance": [
-    {
-      "image": "https://p.ssl.qhimg.com/dm/365_365_/t01b299246b4e983337.jpg",
-      "title": "Dcduiy",
-      "subTitle": "Dnvncing",
-      "note": "3D 搭建设计"
-    },
-    {
-      "image": "https://p.ssl.qhimg.com/dm/365_365_/t01001f8721fd3fc809.jpg",
-      "title": "Dcduiy",
-      "subTitle": "Dnvncing",
-      "note": "3D 搭建设计"
-    }, {
-      "image": "https://p.ssl.qhimg.com/dm/417_417_/t01e4d67a651ab1d53c.jpg",
-      "title": "Dcduiy",
-      "subTitle": "Dnvncing",
-      "note": "3D 搭建设计"
-    }
-  ],
+// 模拟数据，数据格式按照下面的来，如有修改联系我
+var data = [
+  {
+    typeName: "影视资源",
+    types: ['影视分类1', '影视分类2', '影视分类11', '影视分类2'],
+    data: [
+      {
+        image: "https://p.ssl.qhimg.com/dm/664_417_/t01eaff13080a5f2829.jpg",
+        title: "高清KT版,规格9×9 价格面议",
+        logo: "https://p.ssl.qhimg.com/dm/664_417_/t01eaff13080a5f2829.jpg",
+        name: "广州阿迪有限公司",
+        time: " 2 hours ago",
+        read: "1.2W"
+      },
+      {
+        image: "https://p.ssl.qhimg.com/dm/664_417_/t01eaff13080a5f2829.jpg",
+        title: "高清KT版,规格9×9 价格面议",
+        logo: "https://p.ssl.qhimg.com/dm/664_417_/t01eaff13080a5f2829.jpg",
+        name: "广州阿迪有限公司",
+        time: " 2 hours ago",
+        read: "1.2W"
+      },
+    ]
+  }
+  ,
+  {
+    typeName: "影视资源2",
+    types: ['影视分类11', '影视分类21', '影视2分类11', '影视分2类2'],
+    data: [
+      {
+        image: "https://p.ssl.qhimg.com/dm/664_417_/t01eaff13080a5f2829.jpg",
+        title: "高清K2323T版,规格9×9 价格面议",
+        logo: "https://p.ssl.qhimg.com/dm/664_417_/t01eaff13080a5f2829.jpg",
+        name: "广州阿迪有限公司",
+        time: " 2 hours ago",
+        read: "1.2W"
+      },
+      {
+        image: "https://p.ssl.qhimg.com/dm/664_417_/t01eaff13080a5f2829.jpg",
+        title: "高清KT版2323,规格9×9 价格面议",
+        logo: "https://p.ssl.qhimg.com/dm/664_417_/t01eaff13080a5f2829.jpg",
+        name: "广州阿迪有限公司",
+        time: " 2 hours ago",
+        read: "1.2W"
+      },
+    ]
+  }
 
-  "engine": [
-    {
-      "image": "https://p.ssl.qhimg.com/dm/299_417_/t0122af5092acecdb35.jpg",
-      "title": "2018广州分等会啊实打实的",
-      "describe": "Dnvncing"
-    },
-    {
-      "image": "https://ps.ssl.qhimg.com/dm/299_417_/t015dfab25f6d4b6556.jpg",
-      "title": "2018广州分等会啊实打实的",
-      "describe": "Dnvncing"
-    },
-    {
-      "image": "https://p.ssl.qhimg.com/dm/417_417_/t01e4d67a651ab1d53c.jpg",
-      "title": "2018广州分等会啊实打实的",
-      "describe": "Dnvncing"
-    },
-    {
-      "image": "https://p.ssl.qhimg.com/dm/417_417_/t01e4d67a651ab1d53c.jpg",
-      "title": "2018广州分等会啊实打实的",
-      "describe": "Dnvncing"
-    },
-  ],
 
-  "information": [
-    {
-      "image": "http://img4.imgtn.bdimg.com/it/u=2153937626,1074119156&fm=26&gp=0.jpg",
-      "title": "新闻标题",
-      "subTitle": "新闻内容新闻内容新新闻内容新闻内容新闻内容新闻内容新闻内容新闻内容新闻内容新闻内新闻内容新闻内容新闻内容新闻内容新闻内容新闻内容新闻内容新闻内容容新闻内容新闻内容新闻内容新闻内容闻内容新闻内容",
-      "date": "2019/02/23",
-      "time": "18:00"
-    },
-    {
-      "image": "https://p.ssl.qhimg.com/dm/417_417_/t01e4d67a651ab1d53c.jpg",
-      "title": "新闻标题",
-      "subTitle": "新闻内容新闻内容新闻新闻内容新闻内容新闻内容新闻内容新闻内容新闻内容新闻内容新闻内容新闻内容新闻内容新闻内容新闻内容内容新闻内容",
-      "date": "2019/02/23",
-      "time": "18:00"
-    },
-    {
-      "image": "http://img4.imgtn.bdimg.com/it/u=2153937626,1074119156&fm=26&gp=0.jpg",
-      "title": "新闻标题",
-      "subTitle": "新闻内容新闻内容新闻内容新闻内容新闻内容新闻内容新闻内容新闻内容新闻内容新闻内容新闻内容新闻内容新闻内容新闻内容新闻内容新闻内容新闻内容新闻内容新闻内容新闻内容",
-      "date": "2019/02/23",
-      "time": "18:00"
-    }
-  ],
+]
 
-  "project": [
-    {
-      "image": "http://img4.imgtn.bdimg.com/it/u=2153937626,1074119156&fm=26&gp=0.jpg",
-      "time": "1分钟前发布",
-      "name": "项目名称",
-      "describe": "项目名称处处都是彼此变得巨空间发的 出那句发现女款百搭发型看 v创造性cx 从 v 现在更好吃真好v 地方环境中进。"
-    },
-    {
-      "image": "https://p.ssl.qhimg.com/dm/417_417_/t01e4d67a651ab1d53c.jpg",
-      "time": "13分钟前发布",
-      "name": "项目名称2",
-      "describe": "项目名称处处都是彼此变得巨空间发的 出那句发现女款百搭发型看 v创造性cx 从 v 现在更好吃真好v 地方环境中进。"
-    }, {
-      "image": "http://img4.imgtn.bdimg.com/it/u=2153937626,1074119156&fm=26&gp=0.jpg",
-      "time": "12分钟前发布",
-      "name": "项目名称3",
-      "describe": "项目名称处处都是彼此变得巨空间发的 出那句发现女款百搭发型看 v创造性cx 从 v 现在更好吃真好v 地方环境中进。"
-    }, {
-      "image": "http://img4.imgtn.bdimg.com/it/u=2153937626,1074119156&fm=26&gp=0.jpg",
-      "time": "1分钟前发布",
-      "name": "项目名称4",
-      "describe": "项目名称处处都是彼此变得巨空间发的 出那句发现女款百搭发型看 v创造性cx 从 v 现在更好吃真好v 地方环境中进。"
-    }, {
-      "image": "http://img4.imgtn.bdimg.com/it/u=2153937626,1074119156&fm=26&gp=0.jpg",
-      "time": "11分钟前发布",
-      "name": "项目名称5",
-      "describe": "项目名称处处都是彼此变得巨空间发的 出那句发现女款百搭发型看 v创造性cx 从 v 现在更好吃真好v 地方环境中进。"
-    }, {
-      "image": "http://img4.imgtn.bdimg.com/it/u=2153937626,1074119156&fm=26&gp=0.jpg",
-      "time": "15分钟前发布",
-      "name": "项目名称6",
-      "describe": "项目名称处处都是彼此变得巨空间发的 出那句发现女款百搭发型看 v创造性cx 从 v 现在更好吃真好v 地方环境中进。"
-    }
-  ],
-
-  "partnar": [
-    {
-      "image": "http://img4.imgtn.bdimg.com/it/u=2153937626,1074119156&fm=26&gp=0.jpg",
-      "name": "陈田",
-      "describe": "专业3D 30年"
-    },
-    {
-      "image": "http://img4.imgtn.bdimg.com/it/u=2153937626,1074119156&fm=26&gp=0.jpg",
-      "name": "陈田2",
-      "describe": "专业3D 30年"
-    },
-    {
-      "image": "http://img4.imgtn.bdimg.com/it/u=2153937626,1074119156&fm=26&gp=0.jpg",
-      "name": "陈田3",
-      "describe": "专业3D 30年"
-    },
-    {
-      "image": "http://img4.imgtn.bdimg.com/it/u=2153937626,1074119156&fm=26&gp=0.jpg",
-      "name": "陈田4",
-      "describe": "专业3D 30年"
-    },
-    {
-      "image": "http://img4.imgtn.bdimg.com/it/u=2153937626,1074119156&fm=26&gp=0.jpg",
-      "name": "陈田5",
-      "describe": "专业3D 30年"
-    }
-  ],
-
-}
-
-module.exports = data
+module.exports = data;
